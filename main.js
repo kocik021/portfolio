b29914a47ffe515ce2852cd3836e5a9ba8d87e90
Jarek Kocik <kocik1908@gmail.com>
2020-03-01 20:48:46 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+// BURGER
+
+const burger = document.querySelector("nav.burger-side-menu");
+const ico = document.querySelector(".burger-ico i");
+
+// Buttons
+
+const jk = document.querySelector(".JK");
+const aboutBtn = document.querySelector(".dot-about");
+const projectsBtn = document.querySelector(".dot-projects");
+const contactBtn = document.querySelector(".dot-contact");
+const burgerIco = document.querySelector(".burger-ico");
+
+// Sections
+
+const face = document.querySelector(".face");
+const body = document.querySelector("body");
+const about = document.querySelector(".about-me");
+const projects = document.querySelector(".projects");
+const contact = document.querySelector(".contact");
+
+jk.addEventListener("click", function() {
+  face.scrollIntoView({ behavior: "smooth" });
+});
+aboutBtn.addEventListener("click", function() {
+  about.scrollIntoView({ behavior: "smooth" });
+});
+projectsBtn.addEventListener("click", function() {
+  projects.scrollIntoView({ behavior: "smooth" });
+});
+contactBtn.addEventListener("click", function() {
+  contact.scrollIntoView({ behavior: "smooth" });
+});
+
+function animations() {
+  const scrollInt = window.scrollY;
+  if (scrollInt >= 570) {
+    console.log(scrollInt);
+    about.classList.add("show");
+  }
+  if (scrollInt >= 1200) {
+    projects.classList.add("show");
+  }
+  if (scrollInt >= 1700) {
+    contact.classList.add("show");
+  }
+}
+window.addEventListener("scroll", animations);
+
+burgerIco.addEventListener("click", function() {
+  burger.classList.toggle("show");
+  ico.classList.toggle("fa-bars");
+  ico.classList.toggle("fa-times");
+});
